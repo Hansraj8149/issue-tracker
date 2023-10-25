315e6ce27fe35d54fc6e2f1b6868fdb357a057e9
Hansraj8149 <hansrajsaini8149@gmail.com>
2023-10-25 20:10:27 +0530

    Handle Error

--- app/issues/newIssue/page.tsx
+++ app/issues/newIssue/page.tsx
@@ -1,43 +1,58 @@
-'use client'
+"use client";
 
-import SimpleMdeReact from 'react-simplemde-editor'
-import 'easymde/dist/easymde.min.css';
-import { TextField, Button} from '@radix-ui/themes'
-import React from 'react'
-import {Controller, useForm} from 'react-hook-form';
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
+import SimpleMdeReact from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { TextField, Button, Callout } from "@radix-ui/themes";
+import React, { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 interface IssueFormI {
-    title:string;
-    description:string;
+  title: string;
+  description: string;
 }
 
 const NewIssuePage = () => {
+  const router = useRouter();
 
-    const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueFormI>();
 
-    const  {register, control, handleSubmit} = useForm<IssueFormI>();
+  const [error, setError] = useState("");
 
   return (
-    <form className='max-w-xl space-y-3' onSubmit={ handleSubmit(async (data) =>{
-         await axios.post('/api/issues',data);
-         router.push('/issues');
+    <div>
+        {error && (
+        <Callout.Root color="red" className="mb-5">
+            <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>)}
 
-    } )}>
-        <TextField.Root>
-            <TextField.Input placeholder='title' {...register('title')}/>
-        </TextField.Root>
-        <Controller
-        name='description'
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+          try {
+              await axios.post("/api/issues", data);
+              router.push("/issues");
+            } catch (error) {
+                setError("An Expected Error Occured");
+            }
+        })}
+        >
+      <TextField.Root>
+        <TextField.Input placeholder="title" {...register("title")} />
+      </TextField.Root>
+      <Controller
+        name="description"
         control={control}
-        render={({field}) =><SimpleMdeReact placeholder='description' {...field}/> }
-        />
-        
-        <Button>Submit New Issue</Button>
-      
-    </form>
-  )
-}
+        render={({ field }) => (
+            <SimpleMdeReact placeholder="description" {...field} />
+            )}
+            />
 
-export default NewIssuePage
+      <Button>Submit New Issue</Button>
+    </form>
+            </div>
+  );
+};
+
+export default NewIssuePage;
